d50463dfcd1eab056623c7c69a23332e1f909835
azimsyncross <azimsyncross@gmail.com>
2024-11-17 14:20:59 +0600

    feat: add update options

--- src/app/components/todo-item/todo-item.component.ts
+++ src/app/components/todo-item/todo-item.component.ts
@@ -1,19 +1,39 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, Input, Output, EventEmitter, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-todo-item',
   templateUrl: './todo-item.component.html',
 })
-export class TodoItemComponent {
-  @Input() task!: { name: string, completed: boolean };
-  @Output() remove = new EventEmitter<void>();
-  @Output() toggleCompletion = new EventEmitter<void>();
+export class TodoItemComponent implements OnInit {
+  @Input() task!: { id: number, name: string, completed: boolean };
+  @Output() remove = new EventEmitter<number>();
+  @Output() toggleCompletion = new EventEmitter<number>();
+  @Output() update = new EventEmitter<string>();
+
+  isEditing = false;
+  editedTaskName = '';
+
+  ngOnInit() {
+    this.editedTaskName = this.task.name;
+  }
 
   onToggleCompletion() {
-    this.toggleCompletion.emit();
+    this.toggleCompletion.emit(this.task.id);
   }
 
   onRemove() {
-    this.remove.emit();
+    this.remove.emit(this.task.id);
+  }
+
+  startEditing() {
+    this.isEditing = true;
+    this.editedTaskName = this.task.name;
+  }
+
+  saveEdit() {
+    if (this.editedTaskName.trim()) {
+      this.update.emit(this.editedTaskName);
+      this.isEditing = false;
+    }
   }
 }
--- src/app/pages/todo/todo.component.ts
+++ src/app/pages/todo/todo.component.ts
@@ -1,20 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-todo',
   templateUrl: './todo.component.html',
 })
-export class TodoComponent {
+export class TodoComponent implements OnInit {
   taskName = '';
   isTaskNameTouched = false;
   isSubmitting = false;
-  tasks: { name: string, completed: boolean }[] = [];
+  tasks: { id: number, name: string, completed: boolean }[] = [];
+
+  ngOnInit() {
+    // Load tasks from localStorage when the component is initialized
+    const storedTasks = localStorage.getItem('tasks');
+    if (storedTasks) {
+      this.tasks = JSON.parse(storedTasks);
+    }
+  }
 
   addTask() {
     if (this.taskName.trim()) {
       this.isSubmitting = true;
       setTimeout(() => {
-        this.tasks.push({ name: this.taskName.trim(), completed: false });
+        const newTask = {
+          id: Date.now(), // Unique ID using current timestamp
+          name: this.taskName.trim(),
+          completed: false
+        };
+        this.tasks.push(newTask);
+
+        // Save tasks to localStorage
+        localStorage.setItem('tasks', JSON.stringify(this.tasks));
+
+        // Reset form fields
         this.taskName = '';
         this.isTaskNameTouched = false;
         this.isSubmitting = false;
@@ -22,11 +40,24 @@ export class TodoComponent {
     }
   }
 
-  removeTask(index: number) {
-    this.tasks.splice(index, 1);
+  removeTask(taskId: number) {
+    this.tasks = this.tasks.filter(task => task.id !== taskId);
+    localStorage.setItem('tasks', JSON.stringify(this.tasks));
   }
 
-  toggleCompletion(index: number) {
-    this.tasks[index].completed = !this.tasks[index].completed;
+  toggleCompletion(taskId: number) {
+    const task = this.tasks.find(t => t.id === taskId);
+    if (task) {
+      task.completed = !task.completed;
+      localStorage.setItem('tasks', JSON.stringify(this.tasks));
+    }
+  }
+
+  updateTask(taskId: number, newName: string) {
+    const task = this.tasks.find(t => t.id === taskId);
+    if (task && newName.trim()) {
+      task.name = newName.trim();
+      localStorage.setItem('tasks', JSON.stringify(this.tasks));
+    }
   }
 }
